7ca574e9a2106e46a62be30e726f35cd5a63b3de
maxsavinmax <savin.maksim.savin@gmail.com>
2022-08-02 02:47:31 +0300

    module.exports

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  n = n + "";
+  let num = parseInt(n.split("").reverse().join(""));
+  if (num < 0) {
+    return Math.abs(num);
+  } else {
+    return num;
+    }
 }
